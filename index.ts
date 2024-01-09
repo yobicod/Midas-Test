ee85bc26fac12fbca1cea3626a346f2322e09215
yobicod <yobicod.4u@gmail.com>
2024-01-09 20:36:36 +0700

    feat: implement test02

--- index.ts
+++ index.ts
@@ -14,3 +14,37 @@ function getClockAngle(hh_mm: string): number {
 
   return angleDiff;
 }
+
+// TEST_02
+export function getQuestionPart(phrases: string[]): string[] {
+  const possibleAnswer: string[] = [];
+
+  phrases.forEach((phrase) => {
+    for (let i = 0; i < phrase.length; i++) {
+      let compareString: string = '';
+      compareString += phrase[i];
+      for (let j = i + 1; j < phrase.length; j++) {
+        compareString += phrase[j];
+
+        if (phrases.every((p) => p.includes(compareString))) {
+          possibleAnswer.push(compareString);
+        }
+      }
+    }
+  });
+
+  const duplicateWord = findMaxLengthString(possibleAnswer);
+
+  return phrases.map((phrase) => phrase.replace(duplicateWord, ''));
+}
+
+function findMaxLengthString(arr: string[]): string {
+  let maxLengthString = '';
+
+  for (const str of arr) {
+    if (str.length > maxLengthString.length) {
+      maxLengthString = str;
+    }
+  }
+  return maxLengthString;
+}
